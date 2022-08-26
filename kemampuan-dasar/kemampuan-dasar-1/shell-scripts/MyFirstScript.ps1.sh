267d2143490f488e31f261d1f1f3366f4eb26b6b
nisfiii <nisfinuurlailatin254@gmail.com>
2022-08-26 09:22:48 +0700

    day 5

--- kemampuan-dasar/kemampuan-dasar-1/shell-scripts/MyFirstScript.ps1.sh
+++
@@ -1,8 +0,0 @@
-Last login: Fri Aug 19 16:13:47 on console
-
-The default interactive shell is now zsh.
-To update your account to use zsh, please run `chsh -s /bin/zsh`.
-For more details, please visit https://support.apple.com/kb/HT208050.
-apples-MacBook-Pro:~ apple$ echo "Hello World"
-Hello World
-apples-MacBook-Pro:~ apple$ 
