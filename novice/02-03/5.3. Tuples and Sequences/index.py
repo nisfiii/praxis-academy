d4218a1dc3246896658dff09b340e2e625e04592
nisfiii <nisfinuurlailatin254@gmail.com>
2022-08-27 14:01:15 +0700

    day 3

---
+++ novice/02-03/5.3. Tuples and Sequences/index.py
@@ -0,0 +1,12 @@
+t = 12345, 54321, 'hello!'
+print(t[0])
+
+print(t)
+
+# Tuples may be nested: 
+u = t, (1, 2, 3, 4, 5)
+print(u)
+
+# but they can contain mutable objects:
+v = ([1, 2, 3], [3, 2, 1])
+print(v)
